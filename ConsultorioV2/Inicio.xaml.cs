865dd192484a04bd127d7562ca45c80ff255a9f4
Roberto r <rrc110010@outlook.com>
2023-11-22 22:18:40 -0600

    -Pruebas de llenado de datos en el inicio -Se agrego el paquete MaterialDesignThemes

--- ConsultorioV2/Inicio.xaml.cs
+++ ConsultorioV2/Inicio.xaml.cs
@@ -25,7 +25,7 @@ namespace ConsultorioV2
         {
             InitializeComponent();
         }
-        //Prueba de tabla
+        #region Prueba de tabla
         struct Cita
         {
             public string Nombre { get; set; }
@@ -37,29 +37,30 @@ namespace ConsultorioV2
 
         }
 
-        List<Cita> listaCitas = new List<Cita>
-{
-    new Cita { Nombre = "María", Apellido = "Gómez", Motivo = "Consulta general", Hora = "10:30", Fecha = "2023-12-01", Doctor = "Dr. Pérez" },
-    new Cita { Nombre = "Juan", Apellido = "López", Motivo = "Examen médico", Hora = "15:45", Fecha = "2023-12-02", Doctor = "Dra. García" },
-    new Cita { Nombre = "Sofía", Apellido = "Ramírez", Motivo = "Vacunación", Hora = "11:15", Fecha = "2023-12-03", Doctor = "Dr. Rodríguez" },
-    new Cita { Nombre = "Diego", Apellido = "Fernández", Motivo = "Consulta general", Hora = "09:00", Fecha = "2023-12-04", Doctor = "Dra. Sánchez" },
-    new Cita { Nombre = "Laura", Apellido = "Martínez", Motivo = "Seguimiento de tratamiento", Hora = "14:30", Fecha = "2023-12-05", Doctor = "Dr. Pérez" },
-    new Cita { Nombre = "Carlos", Apellido = "Pérez", Motivo = "Chequeo rutinario", Hora = "16:00", Fecha = "2023-12-06", Doctor = "Dra. García" },
-    new Cita { Nombre = "Isabel", Apellido = "Díaz", Motivo = "Examen médico", Hora = "10:45", Fecha = "2023-12-07", Doctor = "Dr. Rodríguez" },
-    new Cita { Nombre = "Alejandro", Apellido = "Martínez", Motivo = "Consulta general", Hora = "13:15", Fecha = "2023-12-08", Doctor = "Dra. Sánchez" },
-    new Cita { Nombre = "Ana", Apellido = "González", Motivo = "Vacunación", Hora = "11:30", Fecha = "2023-12-09", Doctor = "Dr. Pérez" },
-    new Cita { Nombre = "Pedro", Apellido = "Rodríguez", Motivo = "Chequeo rutinario", Hora = "14:00", Fecha = "2023-12-10", Doctor = "Dra. García" },
-    new Cita { Nombre = "María", Apellido = "López", Motivo = "Consulta general", Hora = "09:30", Fecha = "2023-12-11", Doctor = "Dr. Rodríguez" },
-    new Cita { Nombre = "Diego", Apellido = "Gómez", Motivo = "Examen médico", Hora = "15:00", Fecha = "2023-12-12", Doctor = "Dra. Sánchez" },
-    new Cita { Nombre = "Sofía", Apellido = "Pérez", Motivo = "Seguimiento de tratamiento", Hora = "12:45", Fecha = "2023-12-13", Doctor = "Dr. Pérez" },
-    new Cita { Nombre = "Juan", Apellido = "Martínez", Motivo = "Vacunación", Hora = "10:15", Fecha = "2023-12-14", Doctor = "Dra. García" },
-    new Cita { Nombre = "Carlos", Apellido = "Fernández", Motivo = "Chequeo rutinario", Hora = "13:30", Fecha = "2023-12-15", Doctor = "Dr. Rodríguez" },
-    new Cita { Nombre = "Laura", Apellido = "Ramírez", Motivo = "Consulta general", Hora = "16:15", Fecha = "2023-12-16", Doctor = "Dra. Sánchez" },
-    new Cita { Nombre = "Isabel", Apellido = "González", Motivo = "Examen médico", Hora = "09:45", Fecha = "2023-12-17", Doctor = "Dr. Pérez" },
-    new Cita { Nombre = "Alejandro", Apellido = "Díaz", Motivo = "Vacunación", Hora = "11:00", Fecha = "2023-12-18", Doctor = "Dra. García" },
-    new Cita { Nombre = "Ana", Apellido = "Rodríguez", Motivo = "Seguimiento de tratamiento", Hora = "14:45", Fecha = "2023-12-19", Doctor = "Dr. Rodríguez" },
-    new Cita { Nombre = "Pedro", Apellido = "Martínez", Motivo = "Chequeo rutinario", Hora = "12:00", Fecha = "2023-12-20", Doctor = "Dra. Sánchez" }
-};
+        private List<Cita> listaCitas = new List<Cita>
+        {
+            new Cita { Nombre = "María", Apellido = "Gómez", Motivo = "Consulta general", Hora = "10:30", Fecha = "2023-12-01", Doctor = "Dr. Pérez" },
+            new Cita { Nombre = "Juan", Apellido = "López", Motivo = "Examen médico", Hora = "15:45", Fecha = "2023-12-02", Doctor = "Dra. García" },
+            new Cita { Nombre = "Sofía", Apellido = "Ramírez", Motivo = "Vacunación", Hora = "11:15", Fecha = "2023-12-03", Doctor = "Dr. Rodríguez" },
+            new Cita { Nombre = "Diego", Apellido = "Fernández", Motivo = "Consulta general", Hora = "09:00", Fecha = "2023-12-04", Doctor = "Dra. Sánchez" },
+            new Cita { Nombre = "Laura", Apellido = "Martínez", Motivo = "Seguimiento de tratamiento", Hora = "14:30", Fecha = "2023-12-05", Doctor = "Dr. Pérez" },
+            new Cita { Nombre = "Carlos", Apellido = "Pérez", Motivo = "Chequeo rutinario", Hora = "16:00", Fecha = "2023-12-06", Doctor = "Dra. García" },
+            new Cita { Nombre = "Isabel", Apellido = "Díaz", Motivo = "Examen médico", Hora = "10:45", Fecha = "2023-12-07", Doctor = "Dr. Rodríguez" },
+            new Cita { Nombre = "Alejandro", Apellido = "Martínez", Motivo = "Consulta general", Hora = "13:15", Fecha = "2023-12-08", Doctor = "Dra. Sánchez" },
+            new Cita { Nombre = "Ana", Apellido = "González", Motivo = "Vacunación", Hora = "11:30", Fecha = "2023-12-09", Doctor = "Dr. Pérez" },
+            new Cita { Nombre = "Pedro", Apellido = "Rodríguez", Motivo = "Chequeo rutinario", Hora = "14:00", Fecha = "2023-12-10", Doctor = "Dra. García" },
+            new Cita { Nombre = "María", Apellido = "López", Motivo = "Consulta general", Hora = "09:30", Fecha = "2023-12-11", Doctor = "Dr. Rodríguez" },
+            new Cita { Nombre = "Diego", Apellido = "Gómez", Motivo = "Examen médico", Hora = "15:00", Fecha = "2023-12-12", Doctor = "Dra. Sánchez" },
+            new Cita { Nombre = "Sofía", Apellido = "Pérez", Motivo = "Seguimiento de tratamiento", Hora = "12:45", Fecha = "2023-12-13", Doctor = "Dr. Pérez" },
+            new Cita { Nombre = "Juan", Apellido = "Martínez", Motivo = "Vacunación", Hora = "10:15", Fecha = "2023-12-14", Doctor = "Dra. García" },
+            new Cita { Nombre = "Carlos", Apellido = "Fernández", Motivo = "Chequeo rutinario", Hora = "13:30", Fecha = "2023-12-15", Doctor = "Dr. Rodríguez" },
+            new Cita { Nombre = "Laura", Apellido = "Ramírez", Motivo = "Consulta general", Hora = "16:15", Fecha = "2023-12-16", Doctor = "Dra. Sánchez" },
+            new Cita { Nombre = "Isabel", Apellido = "González", Motivo = "Examen médico", Hora = "09:45", Fecha = "2023-12-17", Doctor = "Dr. Pérez" },
+            new Cita { Nombre = "Alejandro", Apellido = "Díaz", Motivo = "Vacunación", Hora = "11:00", Fecha = "2023-12-18", Doctor = "Dra. García" },
+            new Cita { Nombre = "Ana", Apellido = "Rodríguez", Motivo = "Seguimiento de tratamiento", Hora = "14:45", Fecha = "2023-12-19", Doctor = "Dr. Rodríguez" },
+            new Cita { Nombre = "Pedro", Apellido = "Martínez", Motivo = "Chequeo rutinario", Hora = "12:00", Fecha = "2023-12-20", Doctor = "Dra. Sánchez" }
+        };
+        #endregion
 
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
@@ -70,7 +71,7 @@ namespace ConsultorioV2
             timer.Start();
             Reloj.Text = DateTime.Now.ToLongDateString();
 
-            llenarTablaPrueba();
+            LlenarTablaPrueba();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -78,9 +79,14 @@ namespace ConsultorioV2
             Reloj.Text = DateTime.Now.ToLongDateString();
         }
 
-        private void llenarTablaPrueba()
+        private void LlenarTablaPrueba()
         {
             TablaCitas.ItemsSource = listaCitas;
+        }
+
+        private void TablaCitas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
